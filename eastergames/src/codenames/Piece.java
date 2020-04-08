27be93497afa39874a0a4b8d6e3904b0f99cb1ff
Bendiked <63344709+Bendiked@users.noreply.github.com>
2020-04-08 14:51:55 +0200

    Piece class updated with constructor and getters.

--- eastergames/src/codenames/Piece.java
+++ eastergames/src/codenames/Piece.java
@@ -4,10 +4,18 @@ public class Piece {
 	
 	private String name;
 	private char color;
-	private int test;
 	
-	public Piece() {
-		
+	public Piece(String name, char color) {
+		this.name = name;
+		this.color = color;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public char getColor() {
+		return color;
 	}
 
 }
